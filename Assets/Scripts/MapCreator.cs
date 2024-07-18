f7c2c32b26f5004150e34888de6812c1fc799c29
OfficerYoda <officeryoda@gmail.com>
2024-07-18 17:02:03 +0200

    Neighbours and center between them

--- Assets/Scripts/MapCreator.cs
+++ Assets/Scripts/MapCreator.cs
@@ -8,12 +8,13 @@ namespace de.officeryoda {
         [SerializeField] private MapPoint pointPrefab;
         [SerializeField] private float spacing;
 
+
         private void Start() {
-            createMap();
+            CreateMap();
         }
 
-        private void createMap() {
-            if(mapSize.x < 0 || mapSize.y < 0) return;
+        private void CreateMap() {
+            if(mapSize.x <= 0 || mapSize.y <= 0) return;
 
             // Destroy old childs
             foreach(Transform child in transform) {
@@ -23,17 +24,46 @@ namespace de.officeryoda {
             // To make it work with a centered parent
             Vector2 offset = new Vector2(mapSize.x, mapSize.y) / -2f + Vector2.one * spacing / 2f;
 
+            MapPoint[,] mapPoints = new MapPoint[mapSize.x, mapSize.y];
+
             for(int x = 0; x < mapSize.x; x++) {
                 for(int y = 0; y < mapSize.y; y++) {
                     MapPoint point = Instantiate(pointPrefab, transform);
                     point.transform.position = new Vector2(x, y) * spacing + offset;
+
+                    mapPoints[x, y] = point;
+                }
+            }
+
+            AssignNeighbours(mapPoints);
+        }
+
+        private void AssignNeighbours(MapPoint[,] mapPoints) {
+            int[] dx = { -1, 0, 1, 1, 1, 0, -1, -1 };
+            int[] dy = { -1, -1, -1, 0, 1, 1, 1, 0 };
+
+            for(int x = 0; x < mapSize.x; x++) {
+                for(int y = 0; y < mapSize.y; y++) {
+                    MapPoint point = mapPoints[x, y];
+                    point.neighbours = new MapPoint[8];
+
+                    for(int i = 0; i < 8; i++) {
+                        int nx = x + dx[i];
+                        int ny = y + dy[i];
+
+                        if(nx >= 0 && nx < mapSize.x && ny >= 0 && ny < mapSize.y) {
+                            point.neighbours[i] = mapPoints[nx, ny];
+                        } else {
+                            point.neighbours[i] = point; // Refer to itself if out of bounds
+                        }
+                    }
                 }
             }
         }
 
         private void OnValidate() {
             if(Application.isPlaying) {
-                createMap();
+                CreateMap();
             }
 
             if(mapSize.x < 0) mapSize.x = 0;
--- Assets/Scripts/MapPoint.cs
+++ Assets/Scripts/MapPoint.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 namespace de.officeryoda {
     [RequireComponent(typeof(SpriteRenderer))]
-	public class MapPoint : MonoBehaviour {
+    public class MapPoint : MonoBehaviour {
 
-		public Color color = Color.white;
-
+        public Color color = Color.white;
         private SpriteRenderer rend;
+
+        public MapPoint[] neighbours;
+
+        public bool drawGizmos;
 
         private void Start() {
             rend = GetComponent<SpriteRenderer>();
@@ -17,6 +20,22 @@ namespace de.officeryoda {
                 rend = GetComponent<SpriteRenderer>();
             }
             rend.color = color;
+        }
+
+        private void OnDrawGizmos() {
+            if(!drawGizmos) return;
+
+            foreach(MapPoint neighbour in neighbours) {
+                Vector2 center = (transform.position + neighbour.transform.position) / 2f;
+                Gizmos.DrawSphere(center, 0.05f);
+            }
+        }
+
+        private void OnDrawGizmosSelected() {
+            foreach(MapPoint neighbour in neighbours) {
+                Vector2 center = (transform.position + neighbour.transform.position) / 2f;
+                Gizmos.DrawSphere(center, 0.05f);
+            }
         }
     }
 }
